83a4a6a7d016c5d8ce12499651351e50478cf3cd
George Rothon <george.rothon@googlemail.com>
2016-03-18 21:39:57 +0000

    Gui update

---
+++ Assets/Gun Maker/Scripts/Reset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Reset : MonoBehaviour 
+{
+	public string[] items;
+	public Rect Box;
+	public string slectedItem = "None";
+
+	
+	
+	private bool editing = false;
+	
+	private void OnGUI()
+	{	
+
+
+		// device get width
+		// device width / 5
+		// set padding
+		//set positons
+
+		if (GUI.Button(Box, slectedItem))
+		{
+			Application.LoadLevel("Workshop");
+		}
+			
+
+
+	}
+}
